be0e4926e906ab73d2a4abee58cdab41d171f1bd
Mishy-Zimwara <zimwaramitchell@gmail.com>
2022-06-30 15:47:03 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,33 +1,13 @@
 #include <stdio.h>
 
-#include <string.h>
-
-
-
 /**
- *
  *  * main - Entry point
- *
- *   * Description: 'the program's descriptioni'
- *
+ *   * print word to output
  *    * Return: Always 0 (Success)
- *
  *     */
 
-
-
 int main(void)
-
 {
-
-	char str[52];
-
-	strcpy(str, "\"Programming is like building a multilingual puzzle");
-
-
-
-	puts(str);
-
-	return (0);
-
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,45 +1,13 @@
-#include<stdio.h>
-
-
+#include <stdio.h>
 
 /**
- *
  *  * main - Entry point
- *
- *   * Description: 'the program's descriptioni'
- *
+ *   * print to output
  *    * Return: Always 0 (Success)
- *
  *     */
 
-
-
 int main(void)
-
 {
-
-	int intType;
-
-	float floatType;
-
-	long int longint;
-
-	char charType;
-
-	long long int longlongint;
-
-
-
-	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
-
-	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
-
-	printf("Size of a long int: %zu byte(s)\n", sizeof(longint));
-
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongint));
-
-	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
-
-	return (0);
-
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
 }
