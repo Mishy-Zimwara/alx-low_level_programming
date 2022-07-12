b157a45d66893879532a38d892298bf401964591
Mishy-Zimwara <zimwaramitchell@gmail.com>
2022-07-12 13:54:31 -0700

    pointers, arrays and strings

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,11 @@
-#include "main.H"
+#include "main.h"
 
 /**
-* reset_to_98 -> a function to update the reference of a pointer
-* @n: pointer to n to be updated
+* reset_to_98 -> a function to update he reference of a pointer
+* @n: pointer to n that will be updated
 */
+
 void reset_to_98(int *n)
 {
-    *n = 98;
+	*n = 98;
 }
